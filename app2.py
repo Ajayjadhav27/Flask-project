4c7567979d72d12a752530439089061cee831717
Ajay Ajit Jadhav <iamajayjadhav9@gmail.com>
2025-06-10 04:04:08 +0530

    app2.py

---
+++ app2.py
@@ -0,0 +1,31 @@
+from flask import Flask,redirect,render_template,request,url_for
+
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        DS=float(request.form['ds'])
+        total_score=(science+maths+DS)/3
+
+    return redirect(url_for('success', score=int(total_score)))    
+
+@app.route('/success/<int:score>')
+def success(score):
+    res=""
+    if score>=50:
+        res='PASS'
+    else:
+        res='fail'
+
+    return render_template('result.html',result=res)        
+
+if __name__=='__main__':
+    app.run(debug=True)
