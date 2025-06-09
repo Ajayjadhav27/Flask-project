073370bee6d3ceb96eb01244a5cc8df62a4e3841
Ajay Ajit Jadhav <iamajayjadhav9@gmail.com>
2025-06-10 02:54:29 +0530

    Added files

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
+
 app=Flask(__name__)
 
 @app.route('/')
-
 def welcome():
-    return "Welcme guys"
+    return "My new project"
 
-@app.route('/success/<int:score>')
-
+@app.route("/success/<int:score>")
 def success(score):
-    return "The person got "+ str(score)
+    return "The student is passed and he got "+ str(score)
 
 @app.route('/fail/<int:score>')
-
 def fail(score):
-    return "The person is fail and got "+ str(score)
+    return "the student is fail and got "+ str(score)
+
+@app.route('/result/<int:marks>')
+def results(marks):
+    result=''
+    if marks<50:
+        result='fail'
+
+    else:
+        result='success'
+
+    return redirect(url_for(result, score=marks))    
 
 if __name__=='__main__':
     app.run(debug=True)
